11684216cee65cf53a0e2e95c110e615de635b12
MCHL91 <michele.cappai1@gmail.com>
2018-12-01 17:49:51 +1100

    test_1

--- R_scripts/DL_part_1.R
+++ R_scripts/DL_part_1.R
@@ -1,12 +1,13 @@
 # Initialise Keras and TensorFlow 
-
+#this o
 ```{r setup, include = FALSE}
 knitr::opts_chunk$set(echo = TRUE, cache = TRUE)
 
 ##install.packages("keras")
 
-library(keras)
+library(keras) #try noww
 ```
+#tensorflow changes
 
 ```{r setup, eval = F}
 install_keras()
