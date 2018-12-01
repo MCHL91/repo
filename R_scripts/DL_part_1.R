4a0698e8f2e439b042ee7976dffa3058bb709772
MCHL91 <michele.cappai1@gmail.com>
2018-12-01 17:02:04 +1100

    expand folder
    expand folder

---
+++ R_scripts/DL_part_1.R
@@ -0,0 +1,13 @@
+# Initialise Keras and TensorFlow 
+
+```{r setup, include = FALSE}
+knitr::opts_chunk$set(echo = TRUE, cache = TRUE)
+
+##install.packages("keras")
+
+library(keras)
+```
+
+```{r setup, eval = F}
+install_keras()
+```
